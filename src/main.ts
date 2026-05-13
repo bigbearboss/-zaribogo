a52fdf755dc299e02804b08a559f429b29f075a1
bigbearboss <99338977+bigbearboss@users.noreply.github.com>
2026-05-14 02:05:33 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -35,6 +35,7 @@ import industryProfiles from "./engine/data/industryProfiles.json";
 import admCodeMap from "./engine/data/admCodeMap";
 
 
+
 // Apply mode attributes to <html> element immediately so CSS can hide QA-only elements
 applyModeToDocument();
 
